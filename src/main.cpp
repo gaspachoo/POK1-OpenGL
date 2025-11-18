ef9c142d0aa748a08ce972a3ecad634e362ffca1
Gaspachoo <gaspard.lefort@outlook.fr>
2025-11-18 20:59:37 +0100

    make box multicolor

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,20 @@ void processInput(GLFWwindow* window)
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"    gl_Position = vec4(aPos, 1.0);\n"
+"    ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+"    FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 int main()
@@ -103,10 +107,11 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[] = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
+        // pos(x,y,z)   // color(r,g,b)
+        -0.5f,  0.5f, 0.0f,  0.0f,0.0f,1.0f, // top-left blue
+         0.5f,  0.5f, 0.0f,  1.0f,0.0f,0.0f, // top-right red
+         0.5f, -0.5f, 0.0f,  1.0f,1.0f,0.0f, // bottom-right yellow
+        -0.5f, -0.5f, 0.0f,  0.0f,1.0f,0.0f  // bottom-left green
     };
 
     unsigned int indices[] = {
@@ -129,8 +134,11 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // 4. then set the vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
 
     // render loop
@@ -143,7 +151,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-		// draw our first rectangle
+		// draw our colorful rectangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -157,6 +165,7 @@ int main()
 	// Libération des ressources
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
 	// Nettoyage et fermeture
